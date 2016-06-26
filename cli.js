fbb73ce33becfdb7f4b34e219f00c46620d220c1
Gabriel Montalvo <gabe.mon5@outlook.com>
2016-06-25 20:04:51 -0400

    adds help output

--- cli.js
+++ cli.js
@@ -7,18 +7,26 @@ const request = require('request');
 const cheerio = require('cheerio');
 const url = 'http://nsanamegenerator.com';
 
-dns.lookup(url.substring(7), function (err) {
-  if (err && err.code === 'ENOTFOUND') {
-		console.log('Please check your internet connection');
-		process.exit(1);
-	} else {
-    request(url, function (err, res, body) {
-      if (!err && res.statusCode == 200) {
-        let $ = cheerio.load(body);
+if (process.argv[2] === 'help') {
+  console.log('\nUsage');
+  console.log('  $ nsacg\n');
+  console.log('Example\n');
+  console.log('  $ nsacg\n');
+  console.log('  BULLDOZERORGAN\n');
+} else {
+  dns.lookup(url.substring(7), function (err) {
+    if (err && err.code === 'ENOTFOUND') {
+		  console.log('Please check your internet connection');
+		  process.exit(1);
+	  } else {
+      request(url, function (err, res, body) {
+        if (!err && res.statusCode == 200) {
+          let $ = cheerio.load(body);
 
-        console.log(($('body').text()).trim());
-      }
-    });
-  }
-});
+          console.log(($('body').text()).trim());
+        }
+      });
+    }
+  });
+}
 
